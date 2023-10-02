1245f42060b90e447b613eae41f6f3bee56d851f
leo.genuit <leo.genuit@thetribe.io>
2023-10-02 17:27:13 +0200

    add responsive

--- js/index.js
+++ js/index.js
@@ -1,20 +1,58 @@
 import { addShadowClass, removeShadowClass } from "./animation.js";
 
 const possibilities = document.querySelectorAll(".possibilities");
-
-// export function addShadowClass(element) {
-//   element.classList.add("click");
-// }
+const screenElement = document.getElementById("screen");
+let screen = screenElement.innerText;
+let currentInput = "";
 
 possibilities.forEach((possibility) => {
   possibility.addEventListener("mousedown", () => addShadowClass(possibility));
   possibility.addEventListener("mouseup", () => removeShadowClass(possibility));
-  possibility.addEventListener("click", () => getNumScreen(possibility));
+
+  if (isNumeric(possibility.innerText) || possibility.innerText === ".") {
+    possibility.addEventListener("click", () =>
+      appendToScreen(possibility.innerText)
+    );
+  } else if (possibility.innerText === "AC") {
+    possibility.addEventListener("click", clearScreen);
+  } else if (possibility.innerText === "=") {
+    possibility.addEventListener("click", performOperation);
+  }
 });
 
-function getNumScreen(touch) {
-  let screenElement = document.getElementById("screen");
-  console.log("1", screen.innerText);
-  screenElement.innerText = touch.innerText;
-  console.log("2", screen);
+function appendToScreen(value) {
+  if (screen === "0" || screen === "AC") {
+    screen = value;
+  } else {
+    screen += value;
+  }
+  screenElement.innerText = screen;
+}
+
+function clearScreen() {
+  screen = "0";
+  screenElement.innerText = screen;
+}
+
+function performOperation() {
+  // Split the screen content based on the operator
+  const numbers = screen.split("+");
+
+  // Parse the numbers into integers
+  const num1 = parseInt(numbers[0]);
+  const num2 = parseInt(numbers[1]);
+
+  // Perform the addition
+  const result = addition(num1, num2);
+
+  // Display the result on the screen
+  screenElement.innerText = result.toString();
+}
+
+function addition(num1, num2) {
+  return num1 + num2;
+}
+
+function isNumeric(value) {
+  return /^\d+$/.test(value);
 }
